d7a43445a087247f18699a90cab568390b69537b
Alina Salakhova <alina.salakhova1991@gmail.com>
2023-12-10 15:40:44 +0100

    "Added import and call for subtract function in main.py".

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from funcs import add_numbers
+from funcs import add_numbers, subtract
 
 result_addition = add_numbers(5, 3)
 print(f"Result of addition: {result_addition}")
+
+result_subtraction = subtract(8, 3)
+print(f"Result of subtraction: {result_subtraction}")
